77f0c76b32147c3296f2e7c3765137bf7a209be9
0000111213 <arisn1699@gmail.com>
2019-06-27 16:50:54 +0900

    blinkkkkkkkk

--- src/blink.py
+++ src/blink.py
@@ -1,12 +1,15 @@
-import time
 import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT)
+COUNT = 3
+PIN = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN,GPIO.OUT)
 
-time.sleep(1)
+for _ in xrange(COUNT):
+    GPIO.output(PIN,True)
+    time.sleep(1.0)
+    GPIO.output(PIN,False)
+    time.sleep(1.0)
 
-GPIO.output(14, GPIO.HIGH)
-
-time.sleep(1)
-
+GPIO.cleanup()
