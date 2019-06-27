ff9900c0bdbfc70b8ac76525311fc2e5f8bac539
ManamuKitajima <m0228.manacha@gmail.com>
2019-06-27 16:49:21 +0900

    LED

--- src/blink.py
+++ src/blink.py
@@ -1,10 +1,14 @@
+
 import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
+while True:
 GPIO.output(14, GPIO.HIGH)
-time.sleep(10)
-
+time.sleep(1)
+GPIO.output(14, GPIO.LOW)
+time.sleep(1)
 GPIO.cleanup()
+
--- src/light.py
+++ src/light.py
@@ -4,11 +4,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
-while True:
 GPIO.output(14, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(14, GPIO.LOW)
-time.sleep(1)
+time.sleep(10)
+
 GPIO.cleanup()
-
-
