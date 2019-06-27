22beb2d9a10a6e4b9d0f551a75c4d7104ae350df
0000111213 <arisn1699@gmail.com>
2019-06-27 16:43:17 +0900

    blinkkk

--- src/blink.py
+++ src/blink.py
@@ -1,12 +1,12 @@
 import time
 import RPi.GPIO as GPIO
 
-PIN=14
+
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.OUT)
-    GPIO.output(PIN,True)
+GPIO.setup(14, GPIO.OUT)
+    GPIO.output(14,True)
     time.sleep(1.0)
-    GPIO.output(PIN,False)
+    GPIO.output(14,False)
     time.sleep(1.0)
 GPIO.cleanup()
