cbc086e8b85be0c8ddf2becfc5bdc393b5ece0c2
HIYryuni <ryui1013310133@gmail.com>
2019-06-27 16:37:41 +0900

    RYUUI

--- src/blink.py
+++ src/blink.py
@@ -4,8 +4,9 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
-GPIO.output(14, GPIO.HIGH)
-time.sleep(1)
-
-GPIO.cleanup()
+while True:
+    GPIO.output(14, True)
+    time.sleep(1)
+    GPIO.output(14, False)
+    time.sleep(1)
 
