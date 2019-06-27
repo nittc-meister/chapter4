5069fc7ad1f94f5eefe96f422e52739980246386
Kikuchi-Yuto <50942714+Kikuchi-Yuto@users.noreply.github.com>
2019-06-27 17:56:53 +0900

    Update blink.py

--- src/blink.py
+++ src/blink.py
@@ -5,11 +5,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT) 
     
  
- while True: 
-     GPIO.output(14, GPIO.HIGH) 
-     time.sleep(1) 
-     GPIO.output(14, GPIO.LOW) 
-     time.sleep(1) 
+while True: 
+    GPIO.output(14, GPIO.HIGH) 
+    time.sleep(1) 
+    GPIO.output(14, GPIO.LOW) 
+    time.sleep(1) 
     
- GPIO.cleanup() 
+GPIO.cleanup() 
      
