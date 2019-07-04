5a178b0e572361acbd9ee6f894e87cf58528df2e
Your Name <you@example.com>
2019-07-04 17:46:00 +0900

    blink

--- src/light.py
+++ src/light.py
@@ -4,7 +4,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
-GPIO.output(14, GPIO.HIGH)
-time.sleep(10)
+while True:
+     GPIO.output(14, True)
+     time.sleep(0.5)
+     GPIO.output(14, False)
+     time.sleep(0.5)
 
 GPIO.cleanup()
